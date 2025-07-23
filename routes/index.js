2dfd99676b0d338c0808c40ab354ed4f85fdb1e6
NibrasKhalid <156461760+NibrasKhalid@users.noreply.github.com>
2025-07-23 15:26:01 -0400

    post route for form details

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    const success = req.query.success;
+    const error = req.query.error;
+  res.render('index', { title: 'Express', success, error  });
 });
+
+router.post('/', async (req, res) => {
+    const { name, email, review } = req.body;
+
+    const message = `Name: ${name}\nEmail: ${email}\nReview: ${review}`;
+
+    try {
+      console.log('Sending to Discord:\n',message);
+        res.render('index', { success: true });
+    } catch (error) {
+        console.error('Error sending to Discord:', error);
+        res.render('index', { error: true });
+    }
+});
+
 
 module.exports = router;
